4cf1caa89f4d8b192eaee0b5d0be28797b88fea8
Felix Burt <ftaburt@gmail.com>
2025-12-15 17:29:26 +0000

    fixed directory

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="disqco",
     version="0.0.3",
-    packages=find_packages(where="disqco"),
+    packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=["numpy==2.2.4", "qiskit==1.2.4", "qiskit-aer==0.15.1", "qiskit-qasm3-import==0.5.1", "networkx", "matplotlib", "pylatexenc", "jupyter-tikz", "ipykernel", "pytest"],
     python_requires='>=3.11',
